6ef7729924d958ef5f0147dc159dbd68aad35177
Leonardo Sanchez <leonardodicaprio831@yahoo.com>
2014-11-18 09:21:02 -0800

    i can move my player to the right

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,7 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"}
+         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+         {name: "mario", type:"image", src: "data/img/player1.png"},
 
 	/* Atlases 
 	 * @example
@@ -16,7 +18,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        {name: "level01", type: "tmx", src: "data/map/level01.tmx"}
+        {name: "level00", type: "tmx", src: "data/map/level00.tmx"}
 
 	/* Background music. 
 	 * @example
